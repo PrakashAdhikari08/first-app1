e3ebf47e9734e3a48a699b065ffbdf7f9fed07df
Prakash <prakashadhikarijhapali@gmail.com>
2020-09-10 22:50:00 +1000

    front end added

---
+++ src/main/resources/static/precache-manifest.54850cbf8448ecb785e1ed773c08f28a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "753825f74c96a95e0d29e740a621dea4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f86773af2d392f5775",
+    "url": "/static/css/main.19eb48a5.chunk.css"
+  },
+  {
+    "revision": "c08f441e83718e50ce1c",
+    "url": "/static/js/2.6eca37ac.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.6eca37ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f86773af2d392f5775",
+    "url": "/static/js/main.dc3023fb.chunk.js"
+  },
+  {
+    "revision": "b6f7b06a9660cd9d76b1",
+    "url": "/static/js/runtime-main.d06c9ba7.js"
+  },
+  {
+    "revision": "a83803cf5da389063d56043c943fd405",
+    "url": "/static/media/bk.a83803cf.jpg"
+  }
+]);
